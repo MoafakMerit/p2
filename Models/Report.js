b820d08442320e4899e13ef762023c074b5e7ee3
Moafak Mohammed <moafakmohammed@MacBook-Pro-som-tillhor-Moafak.local>
2021-10-29 19:37:41 +0200

    add reminder function

--- Models/Report.js
+++ Models/Report.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const dayjs = require('dayjs');
+nodemailer = require('nodemailer')
+const config = require('config');
 
 const ReportSchema = mongoose.Schema({
     date:{
@@ -22,8 +24,40 @@ const ReportSchema = mongoose.Schema({
     center:{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Center"
+    },
+    nextVisit:{
+        type: Date,
     }
 })
+
+
+ReportSchema.methods.sendEmailReminder = async function(patientEmail){       
+var transporter = nodemailer.createTransport({
+  host: config.get('mail.host'),
+  port: config.get('mail.port'),
+  secure: true,
+  auth: {
+    user: "moafak.merit@gmail.com",
+    pass: config.get('mail.password')
+  }
+});
+
+var mailOptions = {
+  from: "moafak.merit@gmail.com",
+  to: patientEmail,
+  subject: 'Health Center Reminder',
+  text: 'This is a reminder!'
+};
+
+ return transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+}
+
 
 const Report = mongoose.model('Report', ReportSchema)
 
--- controllers/Report.js
+++ controllers/Report.js
@@ -2,12 +2,36 @@
 const { StatusCodes } = require("http-status-codes")
 const jwt = require("jsonwebtoken")
 const Report = require("../Models/Report")
+const nodemailer = require('nodemailer')
+const dateFns = require('date-fns')
 
 // Add Report
 
 const addReport = async (req, res) => {
+    const { nextVisit } = req.body
+    //convert string to date
+    const nextVisitToDate =  Date.parse(nextVisit)
+    const dayInMiloisec = 86400000
+    const dateOfReminder = nextVisitToDate - dayInMiloisec
+    
+    
     const report = await Report.create({ ...req.body })
+    const dateOfReport = Date.parse(report.date)
+    // get the patient of the report
+    const patient = await report.populate('patient', 'email -_id')
+    // get the email of the patient
+    const patientEmail = patient.patient.email
     res.status(StatusCodes.CREATED).json(report)
+     
+    //console.log(dateFns.differenceInMilliseconds(Date(nextVisit), Date(dateOfReport))  )
+    // send reminder one day befor the next visit
+    const timeOut = dateFns.differenceInMilliseconds(dateOfReminder, dateOfReport)
+    console.log( timeOut ) 
+    if(nextVisit){
+        setTimeout(() => {
+            report.sendEmailReminder(patientEmail)
+        }, timeOut);
+    }
 }
 
 // Update Report
@@ -45,13 +69,12 @@ const searchReport = async (req, res) => {
 // Get all Reports
 
 const getAllReports = async (req, res) => {
-    throw new Error("cannot get reports for some reason! :( ")
     const allReports = await Report.find()
     res.status(StatusCodes.OK).json(allReports)
 }
 
 const getAreport = async (req, res) => {
-    const report = await Report.findOne({_id:req.params.id}).select('date text createdBy -_id').populate('patient', 'name -_id').populate('center', 'name -_id').populate('createdBy', 'name -_id')
+    const report = await Report.findOne({_id:req.params.id}).select('date text createdBy -_id').populate('patient', 'name email -_id').populate('center', 'name -_id').populate('createdBy', 'name -_id')
     res.status(StatusCodes.OK).json(report)
 }
 
